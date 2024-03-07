dfb62e77c0f34446ab2e92f14d66674b79f18428
Nimosana <Nicolas.morales39@yahoo.fr>
2024-03-07 02:08:47 -0500

    Basic phaser3 project

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/**
+ * Title of Project
+ * Author Name
+ * 
+ * This is a template. You must fill in the title, author, 
+ * and this description to match your project!
+ */
+
+"use strict";
+
+let config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 800,
+    physics: {
+        default: `arcade`
+    },
+    scene: [Boot, Play]
+};
+
+let game = new Phaser.Game(config);
---
+++ js/scenes/Boot.js
@@ -0,0 +1,13 @@
+class Boot extends Phaser.Scene {
+    constructor() {
+        super({
+            key: `boot`
+        });
+    }
+
+    preload() {
+        this.load.on(`complete`, () => {
+            this.scene.start(`play`);
+        });
+    }
+}
